f5de739b8814e933b196d6b01bb07df8dbe9abe1
Gabicolombo <gabriela-colombo15@outlook.com>
2025-12-21 10:49:26 -0300

    feat: add auth to protect grpc api

--- gapi/error.go
+++ gapi/error.go
@@ -22,3 +22,7 @@ func InvalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) er
 	}
 	return statusDetail.Err()
 }
+
+func unauthenticatedError(err error) error {
+	return status.Errorf(codes.Unauthenticated, "unauthenticated: %v", err)
+}
